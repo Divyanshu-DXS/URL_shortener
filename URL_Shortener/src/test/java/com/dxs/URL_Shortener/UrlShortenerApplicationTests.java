94a30295ab3d97589b72234f224f5ea67519d6f6
Divyanshu-DXS <DivyanshuSharmaDXS@gmail.com>
2026-02-05 16:52:55 -0500

    Project Setup + FrontEnd layout Setup

---
+++ URL_Shortener/src/test/java/com/dxs/URL_Shortener/UrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxs.URL_Shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
